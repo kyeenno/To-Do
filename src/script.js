5a8f49d0f8706d22399a27697b7048f1665aa4f2
dainisstrumpe <dainisstrumpe@startschool.org>
2024-11-14 11:01:54 +0200

    js

--- src/script.js
+++ src/script.js
@@ -1,3 +1,22 @@
 const main = () => {
     // This is an empty function
 }
+
+const taskInput = document.getElementById('task-input');
+const addTaskButton = document.getElementById('add-task-button');
+const taskList = document.getElementById('task-list');
+
+function AddTask() {
+    const taskText = taskInput.value;
+        if (!taskText) {
+            alert("You must enter some kind of task!");
+        }else {
+            const newTask = document.createElement('li');
+            newTask.textContent = taskText;
+            taskList.appendChild(newTask);
+            taskInput.value = '';
+        }
+}
+
+
+addTaskButton.addEventListener('click', AddTask);
